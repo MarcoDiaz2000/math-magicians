c4c6a0132714ee0c2812ff4f1ccc1727e8c4d8e2
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-01 12:53:54 -0600

    Add fetch data from API

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,7 @@ import Screen from './Screen';
 import CalculatorButtons from './CalculatorButtons';
 import './Calculator.css';
 import calculate from '../logic/calculate';
+import Quote from './Quote';
 
 const Calculator = () => {
   const [state, setState] = useState({
@@ -16,6 +17,7 @@ const Calculator = () => {
   };
   return (
     <div className="calculator">
+      <Quote />
       <Screen value={state.next || state.total || '0'} />
       <CalculatorButtons clickHandler={handleClick} />
     </div>
